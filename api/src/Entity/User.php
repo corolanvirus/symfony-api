61f676d91c981dde6b2524d30d81ad2d42b14815
yanus <yanus@macbook-pro-de-yanus.home>
2023-01-15 22:56:33 +0100

    fix(userEntity)add write/read item for zipcode property

--- api/src/Entity/User.php
+++ api/src/Entity/User.php
@@ -54,6 +54,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     private ?string $phone = null;
 
     #[ORM\Column(length: 255, nullable: true)]
+    #[Groups(['write', 'read'])]
     private ?string $zipcode = null;
 
     public function getId(): ?int
